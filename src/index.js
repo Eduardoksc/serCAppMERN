2f6947610d4e1f2b976ec3f0eeafc00a5a721cac
EduardoK <eduardodirectortag@gmail.com>
2023-05-10 00:21:12 -0500

    Creación de carpetas y archivos del proyecto, instalacion de express y nodemon , inicializacion del servidor

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require ('express');
+
+const server = express();
+
+// Configuracion 
+
+
+// Middlewares
+
+// Routes
+
+// Static Files 
+
+
+// Inicio de servidor
+server.listen(3000, () =>{
+    console.log("Estoy conectado al servidor 3000")
+})
